f41e3612a75f96dcd9a87ec4625d1334ce970b2e
Max Hunt <max@huntw3.com>
2019-08-05 13:14:52 +0100

    Randomise pipe heights, make harder

--- pipe_pair.gd
+++ pipe_pair.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 func _ready():
 	$VisibilityNotifier2D.connect("screen_exited", self, "on_exited_screen")
+	$VisibilityNotifier2D.connect("screen_entered", self, "on_entered_screen")
 
 func _physics_process(delta):
 	transform = transform.translated(Vector2(-1.0, 0.0))
 
 func on_exited_screen():
 	transform = transform.translated(Vector2(400.0, 0.0))
+
+func on_entered_screen():
+	transform.origin.y = 80.0 + randf() * 100.0 - 50.0
